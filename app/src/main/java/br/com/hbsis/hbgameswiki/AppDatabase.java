82934d1217bae1e68d5bb0bd3f1ae03ff2b0bead
AndreThlckr <andretheilacker@gmail.com>
2019-11-29 11:14:55 -0300

    Changed property 'exportDatabase' in RoomDatabase

--- app/src/main/java/br/com/hbsis/hbgameswiki/AppDatabase.java
+++ app/src/main/java/br/com/hbsis/hbgameswiki/AppDatabase.java
@@ -3,7 +3,7 @@ package br.com.hbsis.hbgameswiki;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Game.class}, version = 1)
+@Database(entities = {Game.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract GameDao gameDao();
 }
