d964270ce6ceee9c4be93360c0890682164d7ee8
AndreThlckr <andretheilacker@gmail.com>
2019-11-29 11:17:02 -0300

    Added getters, setters and toString() method in class 'Game.java'

--- app/src/main/java/br/com/hbsis/hbgameswiki/Game.java
+++ app/src/main/java/br/com/hbsis/hbgameswiki/Game.java
@@ -33,5 +33,88 @@ public class Game {
     @ColumnInfo(name = "game_is_favorite")
     private boolean isFavorite;
 
+    public int getGId() {
+        return gId;
+    }
 
+    public void setGId(int gId) {
+        this.gId = gId;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    public String getGameDeveloper() {
+        return gameDeveloper;
+    }
+
+    public void setGameDeveloper(String gameDeveloper) {
+        this.gameDeveloper = gameDeveloper;
+    }
+
+    public String getGameDescription() {
+        return gameDescription;
+    }
+
+    public void setGameDescription(String gameDescription) {
+        this.gameDescription = gameDescription;
+    }
+
+    public String getGameTags() {
+        return gameTags;
+    }
+
+    public void setGameTags(String gameTags) {
+        this.gameTags = gameTags;
+    }
+
+    public String getGameRequirements() {
+        return gameRequirements;
+    }
+
+    public void setGameRequirements(String gameRequirements) {
+        this.gameRequirements = gameRequirements;
+    }
+
+    public double getGamePrice() {
+        return gamePrice;
+    }
+
+    public void setGamePrice(double gamePrice) {
+        this.gamePrice = gamePrice;
+    }
+
+    public double getGameRating() {
+        return gameRating;
+    }
+
+    public void setGameRating(double gameRating) {
+        this.gameRating = gameRating;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        isFavorite = favorite;
+    }
+
+    @Override
+    public String toString() {
+        return "Game" + getGId() + ": " + getGameName() +
+                ", desenvolvido por " + getGameDeveloper() +
+                "\n Descrição:" + getGameDescription() +
+                "\n Tags:" + getGameTags() +
+                "\n Requerimentos:" + getGameRequirements() +
+                "\n Preço médio: " + getGamePrice() +
+                " / Score:" + getGameRating() +
+                "\n isFavorite: " + isFavorite() +
+                "\n\n";
+    }
 }
