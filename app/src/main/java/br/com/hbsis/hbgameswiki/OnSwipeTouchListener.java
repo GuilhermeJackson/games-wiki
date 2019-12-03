7976696704a81dd2dc4c6125b09caee35c2a44c6
jmarcoslima <jmarcos663@gmail.com>
2019-12-03 11:31:12 -0300

    Adicionado design e animações a logo

--- app/src/main/java/br/com/hbsis/hbgameswiki/OnSwipeTouchListener.java
+++ app/src/main/java/br/com/hbsis/hbgameswiki/OnSwipeTouchListener.java
@@ -6,7 +6,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 
-public abstract  class OnSwipeTouchListener implements OnTouchListener {
+public abstract  class
+OnSwipeTouchListener implements OnTouchListener {
 
 
     private final GestureDetector gestureDetector;
