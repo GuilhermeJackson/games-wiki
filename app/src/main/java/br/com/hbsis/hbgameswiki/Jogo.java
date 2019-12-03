72ef76ea7810c63e788166269ae40badcc595ea7
GuilhermeJackson <guilherme.lamim96@gmail.com>
2019-12-03 11:30:04 -0300

    Tela - Cadastrar jogo

--- app/src/main/java/br/com/hbsis/hbgameswiki/Jogo.java
+++ app/src/main/java/br/com/hbsis/hbgameswiki/Jogo.java
@@ -1,9 +1,8 @@
 package br.com.hbsis.hbgameswiki;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Jogo extends AppCompatActivity {
 
@@ -11,8 +10,6 @@ public class Jogo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jogo);
-        //Removendo ActionBar
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
+
     }
 }
